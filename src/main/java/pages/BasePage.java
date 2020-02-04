f96cc65b3348e147f9159374f01d2f65e608f7ff
EKiselev <mr.kiseleff@list.ru>
2020-02-04 16:31:34 +0300

    Allure Cucumber !!!!!!!

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+
 import static org.junit.Assert.assertEquals;
 import static util.Init.getDriver;
 
